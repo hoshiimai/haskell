2bf17e4cbaa3563610bbaa2bfd4ffc0b4276d702
hoshiimai <66472870+hoshiimai@users.noreply.github.com>
2023-06-29 08:17:25 +0900

    Update test.hs
    20230629

--- test.hs
+++ test.hs
@@ -21,4 +21,44 @@ quicksort [] = []
 quicksort (x:xs) =   
     let smallerSorted = quicksort [a | a <- xs, a <= x]  
         biggerSorted = quicksort [a | a <- xs, a > x]  
-    in  smallerSorted ++ [x] ++ biggerSorted  
+    in  smallerSorted ++ [x] ++ biggerSorted 
+
+
+
+
+
+
+
+
+
+
+data ErrorType = AlreadyExistsError
+               | DoesNotExistError
+               | AlreadyInUseError
+               | FullError
+               | EOFError
+               | IllegalOperation
+               | PermissionError
+               | UserError
+               deriving (Eq, Show)
+
+identifyError :: String -> Maybe ErrorType
+identifyError "AlreadyExistsError" = Just AlreadyExistsError
+identifyError "DoesNotExistError" = Just DoesNotExistError
+identifyError "AlreadyInUseError" = Just AlreadyInUseError
+identifyError "FullError" = Just FullError
+identifyError "EOFError" = Just EOFError
+identifyError "IllegalOperation" = Just IllegalOperation
+identifyError "PermissionError" = Just PermissionError
+identifyError "UserError" = Just UserError
+identifyError _ = Nothing
+
+main :: IO ()
+main = do
+  putStrLn "Enter an error type:"
+  input <- getLine
+  case identifyError input of
+    Just errorType -> putStrLn $ "Error type identified: " ++ show errorType
+    Nothing -> putStrLn "Invalid error type"
+
+
