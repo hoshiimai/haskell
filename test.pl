2ffbeceb4ad6dbacbe49148db8518e74cc591124
hoshiimai <66472870+hoshiimai@users.noreply.github.com>
2023-12-15 14:21:54 +0900

    test.pl の更新

--- test.pl
+++ test.pl
@@ -7,3 +7,31 @@ system('bash', '-c', 'diff <(sort abc.csv) <(sort def.csv)');
 
 
 map { $_ . (any { $_ == $_ } @ddd ? ' exists' : ' does not exist') } @abc
+
+
+
+
+
+
+use strict;
+use warnings;
+
+sub check_array_reference {
+    my ($array_ref) = @_;
+
+    # grepを使用してS, T, Fのいずれかの文字が含まれているか確認
+    my @result = grep { /[STF]/ } @{$array_ref};
+
+    # 結果を出力
+    if (@result) {
+        print "配列に S, T, F のいずれかの文字が含まれています。\n";
+    } else {
+        print "配列に S, T, F のいずれの文字も含まれていません。\n";
+    }
+}
+
+# テスト用の配列
+my @sample_array = qw(Apple Banana Orange Grape STF_Test);
+
+# サブルーチン呼び出し
+check_array_reference(\@sample_array);
